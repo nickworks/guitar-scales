eb278b51340cebe93c2fdad46f759836827ddde8
Nick Pattison <pattison.nick@gmail.com>
2025-01-11 23:15:42 -0500

    Add more tunings, add method to create tunings
    + instruments::offset_strings() -- can take a standard tuning, an shape,
    an offset amount, and a bool for whether or not to flip to left-handed
    * Instrument::from() -- simplified, moved logic to method above

--- src/instruments.rs
+++ src/instruments.rs
@@ -8,16 +8,10 @@ pub struct Tuning {
     pub strings: Vec<usize>,
 }
 impl Tuning {
-    pub fn from(name: &str, mut strings: Vec<usize>, offset: Vec<usize>) -> Tuning {
-        for i in 0..strings.len() {
-            if i >= offset.len() {
-                break;
-            }
-            strings[i] += offset[i];
-        }
+    pub fn from(name: &str, strings: Vec<usize>) -> Tuning {
         Tuning {
             name: name.to_string(),
-            strings: strings,
+            strings,
         }
     }
     pub fn name(&self) -> &String {
@@ -29,7 +23,7 @@ impl Default for Instrument {
         Self {
             name: "None".to_string(),
             tune_index: 0,
-            tunings: vec!(),
+            tunings: vec![],
         }
     }
 }
@@ -38,7 +32,7 @@ impl Instrument {
         Instrument {
             name: name.to_string(),
             tune_index: 0,
-            tunings: tunings,
+            tunings,
         }
     }
     pub fn strings(&self) -> &Vec<usize> {
@@ -48,40 +42,66 @@ impl Instrument {
         &self.tunings[self.tune_index]
     }
     pub fn none() -> Instrument {
-        Instrument::from("none", vec!())
+        Instrument::from("none", vec![])
     }
     pub fn guitar() -> Instrument {
-        let standard = vec!(4,9,14,19,23,28);
-        Instrument::from("Guitar", vec!(
-            Tuning::from("Standard", standard.to_vec(), vec!()),
-            Tuning::from("Standard (lefty)", vec!(28, 23, 19, 14, 9, 4), vec!()),
-            Tuning::from("Standard D", standard.to_vec(), vec!(10, 10, 10, 10, 10, 10)),
-            Tuning::from("DROP D", standard.to_vec(), vec!(10, 12, 12, 12, 12, 12)),
-            Tuning::from("OPEN D", standard.to_vec(), vec!(10, 12, 12, 11, 10, 10)),
-            Tuning::from("OPEN E", standard.to_vec(), vec!( 0,  2,  2,  1,  0,  0)),
-            Tuning::from("OPEN F", standard.to_vec(), vec!( 8,  8, 10, 10, 10,  8)),
-            Tuning::from("OPEN G", standard.to_vec(), vec!(10, 10, 12, 12, 12, 10)),
-            Tuning::from("OPEN C", standard.to_vec(), vec!( 8, 10, 10, 12, 13, 12)),
-        ))
+        let standard = vec![4, 9, 14, 19, 23, 28];
+        let shape_none = vec![0, 0, 0, 0, 0, 0];
+        let shape_e = vec![0, 2, 2, 1, 0, 0];
+        let shape_a = vec![0, 0, 2, 2, 2, 0];
+        let shape_drop_d = vec![-2, 0, 0, 0, 0, 0];
+        let shape_d = vec![-1, 0, 0, 2, 3, 2];
+        Instrument::from("Guitar", vec![
+                Tuning::from("Standard", offset_strings(&standard, &shape_none, 0, false)),
+                Tuning::from("Standard (lefty)", offset_strings(&standard, &shape_none, 0, true)),
+                Tuning::from("Standard Eb", offset_strings(&standard, &shape_none, -1, false)),
+                Tuning::from("Standard D", offset_strings(&standard, &shape_none, -2, false)),
+                Tuning::from("DROP D", offset_strings(&standard, &shape_drop_d, 0, false)),
+                Tuning::from("OPEN D", offset_strings(&standard, &shape_e, -2, false)),
+                Tuning::from("OPEN E", offset_strings(&standard, &shape_e, 0, false)),
+                Tuning::from("OPEN F", offset_strings(&standard, &shape_a, -4, false)),
+                Tuning::from("OPEN G", offset_strings(&standard, &shape_a, -2, false)),
+                Tuning::from("OPEN A", offset_strings(&standard, &shape_a, 0, false)),
+                Tuning::from("OPEN C", offset_strings(&standard, &shape_d, -3, false)),
+            ],
+        )
     }
     pub fn violin() -> Instrument {
-        Instrument::from("Violin", vec!(
-            Tuning::from("Standard", vec![7,14,21,28], vec!()),
-        ))
+        Instrument::from("Violin", vec![
+            Tuning::from("Standard", vec![7, 14, 21, 28]),
+        ])
     }
     pub fn cello() -> Instrument {
-        Instrument::from("Cello", vec!(
-            Tuning::from("Standard", vec![0,7,14,21], vec!()),
-        ))
+        Instrument::from("Cello", vec![
+            Tuning::from("Standard", vec![0, 7, 14, 21]),
+        ])
     }
     pub fn ukulele() -> Instrument {
-        Instrument::from("Ukulele", vec!(
-            Tuning::from("Standard", vec![7,12,16,21], vec!()),
-        ))
+        Instrument::from("Ukulele", vec![
+            Tuning::from("Standard", vec![7, 12, 16, 21]),
+        ])
     }
     pub fn banjo() -> Instrument {
-        Instrument::from("Banjo", vec!(
-            Tuning::from("Standard", vec![7,14,19,23,26], vec!()),
-        ))
+        Instrument::from("Banjo", vec![
+            Tuning::from("Standard", vec![7, 14, 19, 23, 26]),
+        ])
     }
+}
+fn offset_strings(vec1: &[usize], vec2: &[i32], more:i32, is_left:bool) -> Vec<usize> {
+    let mut offset = more;
+    for i in 0..vec2.len() {
+        while vec2[i] + offset < 0 {
+            offset += 12;
+        }
+    }
+    let mut result = Vec::with_capacity(vec1.len());
+    for i in 0..vec1.len() {
+        let plus = vec2[i] + offset;
+        // println!("{:} + {:} - {:} = {:}", more, vec2[i], offset, plus);
+        result.push(vec1[i] + plus as usize);
+    }
+    if is_left {
+        result.reverse();
+    }
+    return result;
 }
